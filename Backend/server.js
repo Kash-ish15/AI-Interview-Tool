0562885910169232d3a2b8232780e338f8521895
kash-ish15 <wadhwakashish13@gmail.com>
2026-03-19 16:14:08 +0530

    env

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,6 @@ connectToDB()
 app.get("/", (req,res)=>{
     res.send("Server started");
 })
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
