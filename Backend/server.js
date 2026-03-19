938d86e3f511e52651bb760d1ed2e70b90da7dcc
kash-ish15 <wadhwakashish13@gmail.com>
2026-03-19 13:39:18 +0530

    updated backend env config

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
 require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+const app = require("./api/app")
+const connectToDB = require("./api/config/database")
 
 connectToDB()
 
 app.get("/", (req,res)=>{
     res.send("Server started");
 })
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
